8555cc9d0659363e464e5fd4ca96f055455b78b0
Sanan bin Tahir <sananbintahir@gmail.com>
2024-10-10 08:28:36 +0000

    add fixed deduction to tax slabs

--- hrms/payroll/doctype/salary_slip/salary_slip.py
+++ hrms/payroll/doctype/salary_slip/salary_slip.py
@@ -2131,13 +2131,18 @@ def calculate_tax_by_tax_slab(annual_taxable_earning, tax_slab, eval_globals=Non
 		if cond and not eval_tax_slab_condition(cond, eval_globals, eval_locals):
 			continue
 		if not slab.to_amount and annual_taxable_earning >= slab.from_amount:
-			tax_amount += (annual_taxable_earning - slab.from_amount + 1) * slab.percent_deduction * 0.01
+			tax_amount += slab.fixed_deduction + (
+				(annual_taxable_earning - slab.from_amount) * slab.percent_deduction * 0.01
+			)
 			continue
 
 		if annual_taxable_earning >= slab.from_amount and annual_taxable_earning < slab.to_amount:
-			tax_amount += (annual_taxable_earning - slab.from_amount + 1) * slab.percent_deduction * 0.01
-		elif annual_taxable_earning >= slab.from_amount and annual_taxable_earning >= slab.to_amount:
-			tax_amount += (slab.to_amount - slab.from_amount + 1) * slab.percent_deduction * 0.01
+			tax_amount += slab.fixed_deduction + (
+				(annual_taxable_earning - slab.from_amount) * slab.percent_deduction * 0.01
+			)
+		# elif annual_taxable_earning >= slab.from_amount and annual_taxable_earning >= slab.to_amount:
+		# 	tax_amount += (slab.to_amount - slab.from_amount + 1) * slab.percent_deduction * 0.01
+		# 	print("TAX AMOUNT HERE THIRD: ", tax_amount)
 
 	# other taxes and charges on income tax
 	for d in tax_slab.other_taxes_and_charges:
